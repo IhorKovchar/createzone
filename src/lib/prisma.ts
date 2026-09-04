fce7eaed228b87552b876f35ba5f65c856aee5b3
IhorKovchar <kovchar.webdev@gmail.com>
2026-09-04 17:38:56 +0200

    change prisma ts file

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,10 +6,11 @@ const globalForPrisma = globalThis as unknown as {
 }
 
 const adapter = new PrismaMariaDb({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'createzone', 
+    host: process.env.DB_HOST!,
+    port: Number(process.env.DB_PORT!),
+    user: process.env.DB_USER!,
+    password: process.env.DB_PASSWORD!,
+    database: process.env.DB_NAME!,
 })
 
 export const prisma =
